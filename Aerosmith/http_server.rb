b14576f9317c28e9c992be9917edcd1fd9ef70c0
DanisMusicNTI <danis.music@elev.ga.ntig.se>
2023-01-30 09:59:42 +0100

    css funkar!
    Har arbetat med att försöka att få css att kunna parsas, visade sig att min kod letade i en mapp som css filen inte var i

--- Aerosmith/http_server.rb
+++ Aerosmith/http_server.rb
@@ -23,6 +23,11 @@ class HTTPServer
             puts "-" * 40 
 
             # #Er HTTP-PARSER tar emot "data" 
+            mime_types = {
+                "css" => "text/css",
+                "js" => "text/javascript",
+                "html" => "text/html"
+            }
             parseddata = @request_handler.parse_request(data)
             sizeOfData = parseddata.size
             resource = "./html#{parseddata[:resource]}"
@@ -38,23 +43,27 @@ class HTTPServer
             file_ending = resource.split(".").last
             if File.exists?(resource)
                 status = 200
-                html = File.read(resource)
-                p html
-                if html.include? "/main.css"
-                    content = "text/css"
-                else
-                    content = "text/html"
+                if file_ending == "css"
+                    content_type = mime_types["css"]
+                    content = File.read(resource)
+                elsif file_ending == "html"
+                    content_type = mime_types["html"]
+                    content = File.read(resource)
+                elsif file_ending == "js"
+                    content_type = mime_types["js"]
+                    content = File.read(resource)
                 end
             else
                 status = 404
-                html = "Didnt find the resource"
+                content = "Didnt find the resource"
             end
 
+
             session.print "HTTP/1.1 #{status}\r\n"
-            session.print "Content-Type: #{content}\r\n"
-            session.print "Content-Length: #{html.size}\r\n"
+            session.print "Content-Type: #{content_type}\r\n"
+            session.print "Content-Length: #{content.size}\r\n"
             session.print "\r\n"
-            session.print html
+            session.print content
             session.close
         end
     end
